6e35f5d63a619cecb931749a2022f574f93b8783
Harshitkumar Patel <harshitpatel24@gmail.com>
2018-03-08 20:51:44 +0530

    Initial Commit

---
+++ Youtube_Views_Predictor/Youtube_Views_Predictor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Youtube_Views_Predictor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Youtube_Views_Predictor.settings")
+
+application = get_wsgi_application()

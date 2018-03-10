de882a98af6ecadb731331cc4dadcf0428778ca2
Harshitkumar Patel <harshitpatel24@gmail.com>
2018-03-10 14:05:39 +0530

    Change Direct to master

--- Youtube_Views_Predictor/Home/views.py
+++ Youtube_Views_Predictor/Home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
+	print("changes made by Harshit");
 	print("changes made by Jay");
 	print("another change made by Jay");
 	print("final change made by Jay");

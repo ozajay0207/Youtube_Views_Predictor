9dae5dfe915e98d4296f1f110a8dd36d9014b291
Jay Oza <ozajay0207@gmail.com>
2018-03-10 13:33:09 +0530

    another change

--- Youtube_Views_Predictor/Home/views.py
+++ Youtube_Views_Predictor/Home/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
 	print("changes made by Jay");
+	print("another change made by Jay");
     return render(request, 'Home/Home.html')

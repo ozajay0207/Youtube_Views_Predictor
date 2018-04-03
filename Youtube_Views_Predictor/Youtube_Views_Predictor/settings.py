8fe6c2e620ab41e0635188a4e76e05ecb8039e73
Jay Oza <ozajay0207@gmail.com>
2018-04-03 22:28:36 +0530

    alert message

--- Youtube_Views_Predictor/Youtube_Views_Predictor/settings.py
+++ Youtube_Views_Predictor/Youtube_Views_Predictor/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'yvp4',
         'USER': 'postgres',
-        'PASSWORD': 'harshit24',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '',
     }

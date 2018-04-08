ce42442c814be43e61011bb6f614474feb8f7a85
Jay Oza <ozajay0207@gmail.com>
2018-04-08 13:32:10 +0530

    validaiton

--- Youtube_Views_Predictor/users/views.py
+++ Youtube_Views_Predictor/users/views.py
@@ -36,9 +36,9 @@ def Sign_Up(request):
         return render(request, 'users/Users_Dashboard.html', {'User_Detail': User_Detail, })
     else:
         if request.method == 'POST':
-            Email = request.POST.get('val-email')
+            Email = request.POST.get('val-email1')
             Display_Name = request.POST.get('val-display-name')
-            Password = request.POST.get('val-password')
+            Password = request.POST.get('val-password1')
             #           Password_Hash=SHA256.new(Password.encode('utf-8')).hexdigest()
             user1 = users(Email=Email, Display_Name=Display_Name, Password=Password)
             user1.save()
@@ -88,13 +88,22 @@ def register(request):
 
 
 def validate_email(request):
-    # print("validate")
-    Email = request.GET.get('val-email')
+    print("validate")
+
+    Email = request.GET.get("username")
+    test = users.objects.filter(Email=Email).exists()
+    print(test)
+    if test:
+        obj = users.objects.get(Email=Email)
+        print(obj.Password)
+
     data = {
-        'is_taken': users.objects.get(Email__iexact=Email).exists()
+        'is_taken': users.objects.filter(Email=Email).exists()
     }
     if not data['is_taken']:
-        data['error_message'] = 'A user with given Email is not exists.'
+        data['error_message'] = "no such user"
+    else:
+        data['pass'] = obj.Password
     return JsonResponse(data)
 
 
